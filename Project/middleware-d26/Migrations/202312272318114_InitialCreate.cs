890eeed5878ffc8e4f8329c1676fb8257295c452
Alberto Pingo <albertopingobusiness@gmail.com>
2023-12-27 23:45:33 +0000

    Add .sql script
    Use Update-Database or run the script

---
+++ Project/middleware-d26/Migrations/202312272318114_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿namespace middleware_d26.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Applications",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Creation_Dt = c.DateTime(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Containers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(maxLength: 50),
+                        Creation_Dt = c.DateTime(),
+                        Parent = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Data",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Content = c.String(maxLength: 50),
+                        Creation_Dt = c.DateTime(),
+                        Parent = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Subscriptions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Creation_Dt = c.DateTime(),
+                        Parent = c.Int(),
+                        Event = c.String(maxLength: 50),
+                        Endpoint = c.String(maxLength: 50),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Subscriptions");
+            DropTable("dbo.Data");
+            DropTable("dbo.Containers");
+            DropTable("dbo.Applications");
+        }
+    }
+}
